8d60e48e8290297c0070a3a0879e45cc6a1e3b8f
Ronit Chopda <35962550+ronit9586@users.noreply.github.com>
2023-03-24 17:51:46 +0530

    Database connection changed

--- config/database.js
+++ config/database.js
@@ -1,29 +1,11 @@
-// const path = require('path');
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-const parse = require('pg-connection-string').parse;
-const DATABASE_URL = process.env.NODE_ENV == 'production' ? process.env.HEROKU_POSTGRESQL_MAROON_URL : process.env.DATABASE_URL;
-const config = parse(DATABASE_URL);
+const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: false
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    debug: true,
+    useNullAsDefault: true,
   },
-}); 
+});
