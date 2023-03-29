a99443cc89c2c928e822b22c5bbe40740233e404
Ronit Chopda <35962550+ronit9586@users.noreply.github.com>
2023-03-29 17:09:22 +0530

    Changed algorithm

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+const parse = require('pg-connection-string').parse;
+const DATABASE_URL = process.env.NODE_ENV == 'production' ? process.env.HEROKU_POSTGRESQL_MAROON_URL : process.env.DATABASE_URL;
+const config = parse(DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: false
     },
-    useNullAsDefault: true,
+    debug: true,
   },
-});
+}); 
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,11 +1,20 @@
-const path = require('path');
+const parse = require('pg-connection-string').parse;
+const DATABASE_URL = process.env.NODE_ENV == 'production' ? process.env.HEROKU_POSTGRESQL_MAROON_URL : process.env.DATABASE_URL;
+const config = parse(DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
-});
+}); 
